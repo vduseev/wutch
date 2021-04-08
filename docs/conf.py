175202bcc9f3ea5ee7a955b70b575d4b6cf63380
vduseev <vagiz@duseev.com>
2021-04-08 10:58:34 +0200

    Bump version: 1.0.9 → 1.0.10

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = f"{datetime.now().year}, Vagiz Duseev"
 author = "Vagiz Duseev"
 
 # Short version
-version = "1.0.9"
+version = "1.0.10"
 # The full version, including alpha/beta/rc tags
 release = version
 
